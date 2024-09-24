5a5ee18acbe2ed6bdc4d50997fa715dd7189331a
fgitonga <munene2050gitonga@gmail.com>
2024-09-24 11:07:12 +0300

    feat: add function to create file, encode img to it. also text is attached into separate file

---
+++ src/download.go
@@ -0,0 +1,41 @@
+package src
+
+import (
+	"fmt"
+	"image"
+	"image/png"
+	"os"
+	"strings"
+)
+
+func DownloadFiles(img image.Image, art []string,fileName string ) (string,error) {
+
+	outputFileName := strings.Split(fileName,".")[0]
+	outputFileTxt, err := os.Create(outputFileName + ".txt")
+	if err != nil {
+		fmt.Println("Error creating txt file", err)
+		return "", err
+	}
+	outputFilePng, err := os.Create(outputFileName + ".png")
+	if err != nil {
+		fmt.Println("Error creating image file", err)
+		return "", err
+	}
+
+	err = png.Encode(outputFilePng, img)
+	if err != nil {
+		fmt.Println("Error encoding img", err)
+		return "", err
+
+	}
+
+	for _, line := range art {
+		_, err := outputFileTxt.WriteString((line + "\n"))
+		if err != nil {
+			
+			return "", err
+		}
+	}
+
+	return outputFileName,nil
+}
